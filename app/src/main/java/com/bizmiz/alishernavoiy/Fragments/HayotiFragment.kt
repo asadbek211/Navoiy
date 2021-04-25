6aaf1dda34eab37414b4f2a6dcb8f449978603d5
manas1027 <manasametov91@gmail.com>
2021-04-25 17:16:27 +0500

    text edit added

--- app/src/main/java/com/bizmiz/alishernavoiy/Fragments/HayotiFragment.kt
+++ app/src/main/java/com/bizmiz/alishernavoiy/Fragments/HayotiFragment.kt
@@ -6,10 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bizmiz.alishernavoiy.NavoiyDatabase
+import com.bizmiz.alishernavoiy.OnTextSizeChangeListener
 import com.bizmiz.alishernavoiy.R
+import com.bizmiz.alishernavoiy.Settings
+import kotlinx.android.synthetic.main.fragment_hayoti.*
 import kotlinx.android.synthetic.main.fragment_hayoti.view.*
 
-class HayotiFragment : Fragment() {
+@Suppress("UNREACHABLE_CODE")
+class HayotiFragment : Fragment(), OnTextSizeChangeListener {
+
+    private lateinit var settings: Settings
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,7 +24,26 @@ class HayotiFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_hayoti, container, false)
         view.hayoti_ijodi.text = NavoiyDatabase.getInstance(requireContext()).dao().getId1().qiymat
         view.back.setOnClickListener { requireActivity().finish() }
-        return view
 
+        settings = Settings(requireContext())
+        view.btnMinus.setOnClickListener {
+            if (settings.getTextSize() > 12) {
+                settings.decrementTextSize()
+                onTextSizeChanged(settings.getTextSize())
+            }
+        }
+        view.btnPlus.setOnClickListener {
+            if (settings.getTextSize() < 32) {
+                settings.incrementTextSize()
+                onTextSizeChanged(settings.getTextSize())
+            }
+        }
+
+        return view
     }
+
+    override fun onTextSizeChanged(size: Float) {
+        hayoti_ijodi.textSize = size
+    }
+
 }
