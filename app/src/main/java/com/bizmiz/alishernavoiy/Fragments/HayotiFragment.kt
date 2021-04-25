521c546a292e271a795dbc97251576ae957f8451
Asadbek Satimov <asadbeksatimov95@gmail.com>
2021-04-25 04:20:48 +0300

    PxToSp

--- app/src/main/java/com/bizmiz/alishernavoiy/Fragments/HayotiFragment.kt
+++ app/src/main/java/com/bizmiz/alishernavoiy/Fragments/HayotiFragment.kt
@@ -5,20 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bizmiz.alishernavoiy.MainActivity
 import com.bizmiz.alishernavoiy.NavoiyDatabase
 import com.bizmiz.alishernavoiy.R
+import kotlinx.android.synthetic.main.fragment_hayoti.*
 import kotlinx.android.synthetic.main.fragment_hayoti.view.*
 
 class HayotiFragment : Fragment() {
-
+ var px = 20f
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_hayoti, container, false)
         view.hayoti_ijodi.text = NavoiyDatabase.getInstance(requireContext()).dao().getId1().qiymat
-        view.back.setOnClickListener { requireActivity().finish() }
-        return view
+        view.back.setOnClickListener {view.hayoti_ijodi.textSize = PxToSp(px)
+        px+=1f}
 
+        return view
+//        requireActivity().finish()
+    }
+    private fun PxToSp(px:Float): Float {
+        return px / resources.displayMetrics.scaledDensity
     }
 }
--- app/src/main/java/com/bizmiz/alishernavoiy/MainActivity.kt
+++ app/src/main/java/com/bizmiz/alishernavoiy/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.bizmiz.alishernavoiy
 
-import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import com.bizmiz.alishernavoiy.Fragments.*
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -75,4 +72,5 @@ class MainActivity : AppCompatActivity() {
             }
             .create().show()
     }
+
 }
