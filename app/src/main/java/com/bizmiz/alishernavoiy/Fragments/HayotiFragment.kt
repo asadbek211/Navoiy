863b3ee4c86f5b9b67bf293ee0a7a34902761352
Asadbek Satimov <asadbeksatimov95@gmail.com>
2021-04-25 23:37:59 +0300

    textSize control

--- app/src/main/java/com/bizmiz/alishernavoiy/Fragments/HayotiFragment.kt
+++ app/src/main/java/com/bizmiz/alishernavoiy/Fragments/HayotiFragment.kt
@@ -1,31 +1,26 @@
 package com.bizmiz.alishernavoiy.Fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.bizmiz.alishernavoiy.MainActivity
+import androidx.fragment.app.Fragment
 import com.bizmiz.alishernavoiy.NavoiyDatabase
 import com.bizmiz.alishernavoiy.R
-import kotlinx.android.synthetic.main.fragment_hayoti.*
 import kotlinx.android.synthetic.main.fragment_hayoti.view.*
 
 class HayotiFragment : Fragment() {
- var px = 20f
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_hayoti, container, false)
         view.hayoti_ijodi.text = NavoiyDatabase.getInstance(requireContext()).dao().getId1().qiymat
-        view.back.setOnClickListener {view.hayoti_ijodi.textSize = PxToSp(px)
-        px+=1f}
+        view.back.setOnClickListener {
+            requireActivity().finish()
+        }
 
         return view
-//        requireActivity().finish()
     }
-    private fun PxToSp(px:Float): Float {
-        return px / resources.displayMetrics.scaledDensity
-    }
+
 }
