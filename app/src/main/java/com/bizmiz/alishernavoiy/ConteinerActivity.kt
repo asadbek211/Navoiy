13496570fdd7bdd3016850859ac404f8e314c169
Asadbek <72410379+asadbek211@users.noreply.github.com>
2021-04-30 11:27:15 +0300

    Fragment bundle

--- app/src/main/java/com/bizmiz/alishernavoiy/ConteinerActivity.kt
+++ app/src/main/java/com/bizmiz/alishernavoiy/ConteinerActivity.kt
@@ -2,6 +2,7 @@ package com.bizmiz.alishernavoiy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bizmiz.alishernavoiy.Fragments.*
 
 class ConteinerActivity : AppCompatActivity() {
--- app/src/main/java/com/bizmiz/alishernavoiy/Fragments/GazalFragment.kt
+++ app/src/main/java/com/bizmiz/alishernavoiy/Fragments/GazalFragment.kt
@@ -1,16 +1,33 @@
 package com.bizmiz.alishernavoiy.Fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.bizmiz.alishernavoiy.R
-class GazalFragment : Fragment() {
+import kotlinx.android.synthetic.main.fragment_gazal.view.*
+
+class GazalFragment : Fragment(){
+    private val fragment = GazalMatni()
+    private val args = Bundle()
+
     override fun onCreateView(
+
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_gazal, container, false)
+        val view = inflater.inflate(R.layout.fragment_gazal, container, false)
+        view.button1.setOnClickListener {
+            args.putInt("key", 1)
+            fragment.arguments = args
+            requireActivity().supportFragmentManager.beginTransaction()
+                .add(R.id.conteiner, GazalMatni()).commit()
+
+
+        }
+
+        return view
     }
 }
+
